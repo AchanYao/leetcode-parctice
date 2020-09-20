3bc8a1bee318ecffc8ae57b25517fc719b8c8541
achan <yyx1310059081@gmail.com>
2020-09-20 17:52:30 +0800

    ac 646

---
+++ src/package_646/Solution.java
@@ -0,0 +1,30 @@
+package package_646;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author Achan
+ * @date 2020/9/20
+ */
+public class Solution {
+
+    public int findLongestChain(int[][] pairs) {
+        int n = pairs.length;
+        int[] dp = new int[n];
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[0]));
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < pairs.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
